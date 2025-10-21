023cfb369659a1f921fbfbac450d2b50902c76f3
rama1997 <raymondma44@gmail.com>
2025-10-21 15:37:34 -0700

    chore: bump to v.0.10.0

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ async function generateManifest(apiKeys, combine, catalog_order) {
 
 	const manifest = {
 		id: "community.morelikethis",
-		version: "0.9.0",
+		version: "0.10.0",
 		resources: [
 			"catalog",
 			"stream",
