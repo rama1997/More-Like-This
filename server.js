f77c72f397814ec472106c778a37f959c41c3d92
rama1997 <raymondma44@gmail.com>
2026-08-07 08:57:42 -0700

    Bump to v0.11.7

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ async function generateManifest(apiKeys, combine, catalog_order) {
 
 	const manifest = {
 		id: "community.morelikethis",
-		version: "0.11.6",
+		version: "0.11.7",
 		resources: [
 			"catalog",
 			"stream",
